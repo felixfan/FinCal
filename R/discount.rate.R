4d930ce69bb7fb821e4a9ce7dd8b848b8c67f051
Felix <felixfanyh@gmail.com>
2017-04-12 23:39:21 +0800

    update discount.rate

--- R/discount.rate.R
+++ R/discount.rate.R
@@ -5,6 +5,8 @@
 #' @param fv future value
 #' @param pmt payment per period
 #' @param type payments occur at the end of each period (type=0); payments occur at the beginning of each period (type=1)
+#' @param lower the lower end points of the rate of return to be searched.
+#' @param upper the upper end points of the rate of return to be searched.
 #' @seealso \code{\link{fv.simple}}
 #' @seealso \code{\link{fv.annuity}}
 #' @seealso \code{\link{fv}}
@@ -15,7 +17,7 @@
 #' @export
 #' @examples
 #' discount.rate(n=5,pv=0,fv=600,pmt=-100,type=0)
-discount.rate <- function(n,pv,fv,pmt,type=0){
-        uniroot(function(r) fv.simple(r,n,pv) + fv.annuity(r,n,pmt,type)-fv,c(1e-10,1e10))$root
+discount.rate <- function(n,pv,fv,pmt,type=0,lower=0.0001,upper=100){
+        uniroot(function(r) fv.simple(r,n,pv) + fv.annuity(r,n,pmt,type)-fv, lower=lower, upper = upper)$root
 }
 
